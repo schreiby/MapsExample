8244d4fc6ec352735577132419ce3df82ac40184
Schreiby <ettie.schreiber@gmail.com>
2015-07-03 13:15:06 -0400

    Navigating to a specific location

--- app/src/main/java/com/example/ettie/mapsexample/MainActivity.java
+++ app/src/main/java/com/example/ettie/mapsexample/MainActivity.java
@@ -4,14 +4,26 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap;
 
 public class MainActivity extends ActionBarActivity {
+
+    private GoogleMap map;
+    private static final LatLng LONDON = new LatLng(+51.5000, -0.11670);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        map = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(LONDON, 15));
+        map.addMarker(new MarkerOptions().position(LONDON));
     }
 
     @Override
