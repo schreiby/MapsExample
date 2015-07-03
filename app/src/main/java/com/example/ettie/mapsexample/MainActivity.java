004a663e1ffaed6644b6f07211d191c406b3b598
Schreiby <ettie.schreiber@gmail.com>
2015-07-03 13:25:37 -0400

    Getting a Touched Location

--- app/src/main/java/com/example/ettie/mapsexample/MainActivity.java
+++ app/src/main/java/com/example/ettie/mapsexample/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.ettie.mapsexample;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     private GoogleMap map;
     private static final LatLng LONDON = new LatLng(+51.5000, -0.11670);
@@ -25,28 +25,13 @@ public class MainActivity extends ActionBarActivity {
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(LONDON, 15));
         map.addMarker(new MarkerOptions().position(LONDON));
 
-        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+       map.setOnMapClickListener(new OnMapClickListener() {
+           @Override
+           public void onMapClick(LatLng latLng) {
+               Toast.makeText(MainActivity.this, "Location: " + latLng, Toast.LENGTH_SHORT).show();
+           }
+       });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
